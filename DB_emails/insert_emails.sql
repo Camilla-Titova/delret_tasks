1e0a5256aba79d838aa502bf07578d66a1b71e8f
_Kamilla_ <71847866+Camilla-Titova@users.noreply.github.com>
2023-05-21 23:57:02 +0300

    Create insert_emails.sql

---
+++ DB_emails/insert_emails.sql
@@ -0,0 +1,3 @@
+insert into email_users.user_info (user_name, user_lastname, email) values ('Kamilla', 'Titova', 'cam.titova@gmail.com');
+insert into email_users.user_info (user_name, user_lastname, email) values ('Kamilla', 'Titova', 'kamilla.titova@outlook.com');
+insert into email_users.user_info (user_name, user_lastname, email) values ('Kamilla', 'Titova', 'kami.titova1303@gmail.com');
